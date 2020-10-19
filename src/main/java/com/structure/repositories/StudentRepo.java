fe64360b1557f402d6979422d6282a4535e3f105
Alan Hunt <34352035+alpal309@users.noreply.github.com>
2020-10-18 20:26:58 -0500

    Folder restructure

--- src/main/java/com/structure/repositories/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.structure.repositories;
-
-import com.structure.models.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepo extends CrudRepository<Student, String> {
-}
--- src/main/java/com/structure/repositories/ThumbnailRepo.java
+++
@@ -1,9 +0,0 @@
-package com.structure.repositories;
-
-import com.structure.models.Thumbnail;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ThumbnailRepo extends CrudRepository<Thumbnail, String> {
-}
